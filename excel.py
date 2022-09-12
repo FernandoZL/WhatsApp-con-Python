310131166f04fe235a2e5dae37ac6ab8a267d37f
Fernando Zuñiga <fernando.zuniga@mcdcentral.local>
2022-09-12 16:31:22 -0600

    pruebas

---
+++ excel.py
@@ -0,0 +1,7 @@
+import pyautogui as pg
+import webbrowser as web
+import time 
+import pandas as pd
+
+
+data = pd.read_excel('C:\\Users\\fernando.zuniga\\Documents\\GitHub\\WhatsApp con Python\\Nuevo.xlsx')
