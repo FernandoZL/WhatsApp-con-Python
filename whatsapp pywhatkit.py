4388b8ab5ee62badfa119d875c3a0e26660c08d1
Fernando Zuñiga <fernando.zuniga@mcdcentral.local>
2022-10-09 20:29:18 -0600

    1
    prueba

--- whatsapp pywhatkit.py
+++ whatsapp pywhatkit.py
@@ -1,7 +1,7 @@
 import pywhatkit
 #numero_telefono = imput("Ingresa numero de telefono: ")
 grupo = "EIxwutlXLW8CsO4GHFx8gB"
-pywhatkit.sendwhatmsg_to_group(grupo,"Este es un mensaje de prueba para enviar mensajes a grupos",21,41)
+pywhatkit.sendwhatmsg_to_group(grupo,"Este es un mensaje de prueba para enviar mensajes a grupos HOY JUEVES A LAS 10:17PM",22,17)
 
 #pywhatkit.sendwhatmsg_group("EIxwutlXLW8CsO4GHFx8gB","mensaje de prueba",16,28)
 
